0c4518f33068444856c86126bca8a7cc6d7b7b04
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2023-05-23 14:47:33 +0300

    Update startup.py

--- zthon/utils/startup.py
+++ zthon/utils/startup.py
@@ -130,7 +130,7 @@ async def mybot():
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", "Tepthon")
+            await bot.send_message("@BotFather", "adthon")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setname")
             await asyncio.sleep(1)
