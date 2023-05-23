6fd7a9ec99f2fa5547a3839afc9fdd0c46245d3b
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2023-05-23 15:28:17 +0300

    Update الهمسه.py

--- zthon/plugins/الهمسه.py
+++ zthon/plugins/الهمسه.py
@@ -1,19 +1,45 @@
 from telethon import events
-from asyncio import sleep
-import os, sys, random, re
-from zthon import zedub
-from ..core.managers import edit_delete, edit_or_reply
- 
+import random, re
+from wasit_go.utils import admin_cmd
+import asyncio 
 
-@zedub.zed_cmd(pattern="همسه ?(.*)")
+@borg.on(
+    admin_cmd(pattern="همسة ?(.*)")
+)
 async def wspr(event):
     if event.fwd_from:
         return
-    wwwspr = event.pattern_match.group(1)
-    botusername = "@ocbot"
+    jepiqb = event.pattern_match.group(1)
+    rrrd7 = "@nnbbot"
     if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, wwwspr)
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(rrrd7, jepiqb) 
     await tap[0].click(event.chat_id)
     await event.delete()
     
+@borg.on(admin_cmd("الهمسه"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("👻︙ اوامر الهمسه واكس او \n\n⌔︙الامر  • `.همسة`\n⌔︙الاستخدام  • لكتابة همسه سرية لشخص في المجموعه \n\n👻︙ الامر • `.الهمسة`\n👻︙ استخدامه • لعرض كيفية كتابة همسة سرية\n\n👻︙ الامر • `.اكس او `\n 👻︙ استخدامه • ففط ارسل الامر لبدء لعبة اكس او\n\n👻︙ CH  - @adthon_help")
+        
+@borg.on(admin_cmd("الهمسة"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("**👻︙ شـرح كيـفية كـتابة همـسة سـرية**\n👻︙ اولا اكتب الامر  .همسة  بعدها الرسالة بعدها اكتب معرف الشخص\n👻︙ مـثال  :   `.همسة ههلا @co_od`")
+        
+@borg.on(
+    admin_cmd(
+       pattern="اكس او$"
+    )
+)
+
+async def gamez(event):
+    if event.fwd_from:
+        return
+    jmusername = "@xoBot"
+    uunzz = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(jmusername, uunzz)
+    await tap[0].click(event.chat_id)
+    await event.delete()
